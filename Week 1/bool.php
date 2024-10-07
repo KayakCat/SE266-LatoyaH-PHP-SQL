64ebe0bcf7f79aba2d8a61f7eb86b40eb476d910
KayakCat <lmhall@email.neit.edu>
2024-10-07 10:03:15 -0400

    booleans & conditionals
    week 1 homework assignment

---
+++ Week 1/bool.php
@@ -0,0 +1,12 @@
+<?php 
+
+$task = [
+    'title' => 'Finish Homework',
+    'due' => 'today',
+    'assigned_to' => 'Jeffrey',
+    'completed' => true
+];
+
+require 'index.view.php';
+?>
+<?php include 'includes/footer.php'; ?>
---
+++ Week 1/index.view.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Task for the Day</h1>
+    <ul>
+        
+        <li>
+            <strong>Name: </strong><?= $task['title'] ?>
+        </li>
+
+        <li>
+            <strong>Due Date: </strong><?= $task['due'] ?>
+        </li>
+
+        <li>
+            <strong>Person Responsible: </strong><?= $task['assigned_to'] ?>
+        </li>
+
+        <li>
+            <strong>Status: </strong>
+
+            <?php
+                if (! true) {}
+
+            ?>
+            
+            <?php if ($task['completed']) : ?>
+
+                <span class="icon">&#9989;</span>
+
+            <?php else :  ?>
+
+                <span class="icon">Incomplete</span>
+            <?php endif; ?>
+        </li>
+       
+    </ul>
+</body>
+</html>
