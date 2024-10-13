7d9e10de46c2956e18a53ae90598d33691751360
KayakCat <lmhall@email.neit.edu>
2024-10-13 12:46:21 -0400

    coursework page

---
+++ Coursework Page/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latoya Hall's PHP Course Page</title>
+</head>
+<body>
+    <h1> Fall 2024 PHP Coursework </h1>
+    <h2> Latoya Hall </h2>
+
+    <p>GitHub Repo: <a href="https://github.com/KayakCat/SE266-LatoyaH-PHP-SQL/tree/main" target="_blank">My GitHub Repo</a></p>
+
+   
+    <h3>PHP Learning Resources</h3>
+    <ul>
+        <li><a href="https://www.codecademy.com/catalog/language/php" target="_blank">Codecademy</a></li>
+        <li><a href="https://www.w3schools.com/php/" target="_blank">W3Schools PHP Tutorial</a></li>
+        <li><a href="https://phptherightway.com/" target="_blank">PHP The Right Way</a></li>
+    </ul>
+
+    <h3>Git Learning Resources</h3>
+    <ul>
+        <li><a href="https://git-scm.com/doc" target="_blank">Git Documentation</a></li>
+        <li><a href="https://www.atlassian.com/git/tutorials" target="_blank">Atlassian Git Tutorials</a></li>
+        <li><a href="https://www.w3schools.com/git/" target="_blank">W3Schools Git Tutorial</a></li>
+    </ul>
+
+    <h3>My personal hobbies</h3>
+    <ul>
+        <li><a href="https://www.rimonthly.com/six-best-spots-for-kayaking-in-rhode-island/" target="_blank">Kayaking</a></li>
+        <li><a href="https://www.alltrails.com/" target="_blank">Hiking</a></li>
+        <li><a href="https://www.nal.usda.gov/plant-production-gardening/vegetable-gardening" target="_blank">Gardening</a></li>
+    </ul>
+
+
+    
+    <h3>Lab Solutions</h3>
+    <ul>
+        <li><a href="http://localhost/SE266-PHP-SQL/Coursework%20page/index.php" target="_blank">Week 2 - Coursework Page</a></li>
+        <li><a href="" target="_blank">Week 3</a></li>
+        <li><a href="" target="_blank">Week 4</a></li>
+        <li><a href="" target="_blank">Week 5</a></li>
+        <li><a href="" target="_blank">Week 6</a></li>
+        <li><a href="" target="_blank">Week 7</a></li>
+        <li><a href="" target="_blank">Week 8</a></li>
+        <li><a href="" target="_blank">Week 9</a></li>
+        <li><a href="" target="_blank">Week 10</a></li>
+    </ul>
+
+    <?php include 'includes/index_footer.php'; ?>
+
+</body>
+</html>
